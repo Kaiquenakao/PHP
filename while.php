37e19c9cb8e1d8a749b2c7d943eb282fa07e3aa3
Kaique nakao <kaiquenakao@outlook.com>
2020-03-13 20:18:23 -0300

    Versão 0.1

---
+++ while.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+
+<title>While</title>
+</head>
+<body>
+<?php
+    $i = 1;
+    while($i < 10){
+        echo "Olá " . "<br/>";
+        $i++;
+    }
+
+
+?>
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+</html>
